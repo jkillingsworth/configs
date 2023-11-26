79a23a41d07940fa820a54db911429d2cfcdb19d
Jim Killingsworth <jkillingsworth@gmail.com>
2023-11-25 20:23:43 -0800

    Update powershell profile

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,4 +1,21 @@
 function Prompt
 {
-    "PS $($executionContext.SessionState.Path.CurrentLocation)`n$('>' * ($nestedPromptLevel + 1)) ";
+    "PS $($executionContext.SessionState.Path.CurrentLocation)`n$('>' * ($nestedPromptLevel + 1)) "
+}
+
+function Out-Default
+{
+    $items = New-Object System.Collections.Generic.List[object]
+
+    foreach ($item in $input)
+    {
+        $items.Add($item)
+    }
+
+    if (-Not ($items | Out-String).ReplaceLineEndings("`n").EndsWith("`n`n"))
+    {
+        $items.Add([System.String]::Empty)
+    }
+
+    $items | Microsoft.PowerShell.Core\Out-Default
 }
